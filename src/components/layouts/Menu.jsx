24b6dbcb6d74878d067237d8308487e99ec5bf2c
unknown <Lucasso461@gmail.com>
2026-01-08 17:12:32 -0300

    Começando Menu

--- src/components/layouts/Menu.jsx
+++ src/components/layouts/Menu.jsx
@@ -1,9 +1,14 @@
-import {Link} from "react-router-dom"
+import MenuIcon from "../icons/MenuIcon"
+import CloseIcon from "../icons/CloseIcon"
+import {useState} from "react"
 function Menu(){
+    const [menu,setMenu]=useState(false)
     return(
         <>
-            <div className="absolute ring-1 text-white w-dvw h-30">
-                <Link to="/home">Home</Link>
+            <div className="absolute h-24 text-black bg-white w-full justify-around flex">
+                <img src="./contactus_logo.png"></img>
+                <MenuIcon className={`size-9 mt-7 right-6 absolute transition-all duration-300 ${menu?"opacity-100":"opacity-0 pointer-events-none rotate-45"}`} onClick={()=>setMenu(false)}/>
+                <CloseIcon className={`size-9 mt-7 right-6 absolute transition-all duration-300 ${menu?"opacity-0 pointer-events-none rotate-45":"opacity-100"}`} onClick={()=>setMenu(true)}/>
             </div>
         </>
     )
