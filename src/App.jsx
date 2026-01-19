40c12f87ba17948486a89f74566a00f8c6853263
unknown <Lucasso461@gmail.com>
2026-01-19 17:05:59 -0300

    Começando login

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <div className="absolute w-full h-full bg-gradient-to-l from-blue-300 to-green-300 flex flex-col gap-10">
+          <h1 className="mt-20 text-4xl flex items-center justify-center">Login</h1>
+          <div className="flex flex-col">
+          <label className="items-center justify-center flex">Email:</label>
+          <input type="text" className="border w-[75%] mx-auto outline-none p-1"/>
+          </div>
+          <div className="flex flex-col">
+          <label className="flex items-center justify-center">Senha:</label>
+          <input type="text" className="border w-[75%] mx-auto outline-none p-1"/>
+          </div>
+          <button className="bg-blue-400 h-10 w-[60%] mx-auto rounded-xl font-semibold">Logar</button>
+      </div>
     </>
   )
 }
--- src/components/icons/PerfilIcon.jsx
+++ src/components/icons/PerfilIcon.jsx
@@ -2,7 +2,7 @@ export default function PerfilIcon({className,onClick}){
     const initial="L"
     return(
         <div className={`${className} flex items-center justify-center`}>
-            <div onClick={onClick} className="ring-2 ring-black w-full h-12 rounded-4xl flex items-center justify-center text-xl font-bold font-mono">{initial}</div>
+            <div onClick={onClick} className="ring-2 ring-blue-500 text-blue-500 w-full h-12 rounded-4xl flex items-center justify-center text-xl font-bold font-mono">{initial}</div>
         </div>
     )
 }
